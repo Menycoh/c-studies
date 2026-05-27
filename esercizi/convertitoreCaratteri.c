73c40a8c0ec1e2d9c2fc856320d9abc0483a5aa3
Menyco <menycoz@proton.me>
2026-05-27 03:37:47 +0200

    Aggiunto  nuovo esercizio

---
+++ esercizi/convertitoreCaratteri.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    char lettera;
+    printf("Inserisci un carattere: ");
+scanf("%c",&lettera);
+    printf("Il carattere ASCCI inserito è: ",lettera);
+    printf("Il valore numerico del carattere è: %d",lettera);
+
+    return 0;
+}
